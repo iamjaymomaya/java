2d05c73f0e30daeb3175081e696c8e84a3042407
Jay Momaya <jk07momaya@gmail.com>
2024-12-30 23:59:17 +0530

    studying primitive data types

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,7 @@
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        System.out.println("Integer range is (" + Integer.MIN_VALUE + " <=> " + Integer.MAX_VALUE + ")");
+
+        System.out.println("Float range is (" + Float.MIN_VALUE + " <=> " + Float.MAX_VALUE + ")");
+    }
+}
--- PrintNumber.java
+++ PrintNumber.java
@@ -1,6 +1,6 @@
 public class PrintNumber {
     public static void main(String[] args) {
-        int myFirstNumber = 5;
+        int myFirstNumber = 100000000;
         System.out.print(myFirstNumber);
     }
 }
