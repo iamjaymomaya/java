8fc8e2fbe5ff13ac1754c42061da52078903f24f
Jay Momaya <jk07momaya@gmail.com>
2024-12-30 23:08:29 +0530

    first code of java

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("Hello, Jay!");
+        System.out.print("Hello, Momaya!");
+    }
+}
